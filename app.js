e8d66fe6b7f0370dc3bfa4e5b97cb0cb42757be9
Abhang Pawar <abhangpawar03@gmail.com>
2025-03-22 17:35:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000 , (req,res)=>{
+    console.log('Server is running on port 3000');
+})
+
+//ejs
+app.set("view engine","ejs");
+
+app.get('/' , (req,res)=>{
+    res.send('Hello World');
+})
