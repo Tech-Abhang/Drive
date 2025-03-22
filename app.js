f8a813e38d1252ba0f5add18485cef65392ab6bf
Abhang Pawar <abhangpawar03@gmail.com>
2025-03-22 18:58:10 +0530

    mongodb connection

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const userRouter = require("./routes/user.routes.js");
+const dotenv = require('dotenv');
+
+//env
+dotenv.config();
+
+//connect to db
+const connectToDB = require("./config/db.js");
+connectToDB();
 
 app.listen(3000 , (req,res)=>{
     console.log('Server is running on port 3000');
 })
+
 
 //ejs
 app.set("view engine","ejs");
