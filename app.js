94059f0d555f62096d4968a6584408396a6e3b4b
Abhang Pawar <abhangpawar03@gmail.com>
2025-05-06 00:44:13 +0530

    updated

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const userRouter = require("./routes/user.routes.js");
 const dotenv = require('dotenv');
-
+const cookieParser = require('cookie-parser');
+const indexRouter = require("./routes/index.routes.js");
 
 //env
 dotenv.config();
+
+//cookie
+app.use(cookieParser());
 
 //connect to db
 const connectToDB = require("./config/db.js");
@@ -24,3 +28,6 @@ app.use(express.json());
 
 //router
 app.use('/user',userRouter);
+
+//index router
+app.use('/',indexRouter);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const userModels = require("../models/user.model.js");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.get('/test' , (req,res)=>{
     res.send('test');
@@ -32,7 +33,46 @@ router.post('/register',
         username : username,
         password : hashedPassword
     })
-    res.send(newUser);
+    res.json(newUser);
+})
+
+router.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+router.post('/login',
+    check('username').trim().isLength({min:3}),
+    check('password').trim().isLength({min:5}),
+    async (req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array(),message:"invalid data"});
+    }
+    const {username,password} = req.body;
+    const user = await userModels.findOne({
+        username:username
+    })
+    if(!user){
+        return res.status(400).json({message:"username incorrect"});
+    }
+    console.log("Input password:", password);
+    console.log("Stored hash:", user.password);
+
+    const inMatch = await bcrypt.compare(password,user.password);
+    if(!inMatch){
+        return res.status(400).json({message:"username or password is incorrect"});
+    }
+    
+    const token = jwt.sign({
+        userID:user._id,
+        email:user.email,
+        username:user.username
+    },
+    process.env.JWT_SECRET,
+)
+    res.cookie("token",token)
+    res.send("login success");
 })
 
 module.exports = router;
